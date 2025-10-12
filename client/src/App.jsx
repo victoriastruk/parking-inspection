73a3913c2f3e662aa10898389dedb16927dad1a0
Viktoriia <struk.victoria@gmail.com>
2025-10-12 17:05:00 +0300

    Add routing to App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import ParkOfficersPage from "./pages/ParkOfficersPage/ParkOfficersPage";
 import ProtocolsPage from "./pages/ProtocolsPage/ProtocolsPage";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    // <ParkOfficersPage />;
-    <ProtocolsPage />
+    <BrowserRouter>
+      <nav>
+        <ul className={styles.navList}>
+          <li>
+            <Link className={styles.link} to="/">Officers</Link>
+          </li>
+          <li>
+            <Link className={styles.link} to="/protocols">Protocols</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<ParkOfficersPage />} />
+        <Route path="/protocols" element={<ProtocolsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
