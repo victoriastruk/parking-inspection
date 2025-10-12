9a421b54aaf8c8321da5d9606e4ab105c54cf5cc
Viktoriia <struk.victoria@gmail.com>
2025-10-12 16:44:48 +0300

    Add slider

--- client/src/components/Protocol/Protocol.jsx
+++ client/src/components/Protocol/Protocol.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-import styles from './Protocol.module.scss'
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styles from "./Protocol.module.scss";
 
 const Protocol = ({ protocol }) => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+  };
+
   return (
     <article className={styles.cardWrapper}>
       <h1>Protocol № {protocol.id}</h1>
@@ -14,14 +26,18 @@ const Protocol = ({ protocol }) => {
 
       <p>Officer full name: {protocol.parkOfficer.full_name}</p>
       <p>Officer badge number: {protocol.parkOfficer.badge_number}</p>
-
-      {protocol.images.map((currentImage) => (
-        <img
-          key={currentImage.id}
-          src={`http://localhost:5001/images/${currentImage.path}`}
-          alt={protocol.id}
-        />
-      ))}
+      {protocol.images.length > 0 && (
+        <Slider {...settings} className={styles.slider}>
+          {protocol.images.map((currentImage) => (
+            <img
+              className={styles.img}
+              key={currentImage.id}
+              src={`http://localhost:5001/images/${currentImage.path}`}
+              alt={protocol.id}
+            />
+          ))}
+        </Slider>
+      )}
     </article>
   );
 };
