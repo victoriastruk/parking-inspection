905ad5697703962497bf259360fc17f9cafefe42
Viktoriia <struk.victoria@gmail.com>
2025-10-20 10:04:44 +0300

    Update filter for protocols

--- client/src/pages/ProtocolsPage/ProtocolsPage.jsx
+++ client/src/pages/ProtocolsPage/ProtocolsPage.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllProtocols } from "../../redux/slices/protocolsSlice";
 import Protocol from "../../components/Protocol/Protocol";
 
-
 const ProtocolsPage = () => {
   const { protocols, isLoading, error } = useSelector(
     (state) => state.protocols
@@ -22,19 +21,47 @@ const ProtocolsPage = () => {
   if (error) {
     return <div>ERROR HAPPENED</div>;
   }
-  const filteredProtocols = protocols.filter(
-    ({
-      violatorFullName,
-      violatorPassportNumber,
-      parkOfficer: { full_name, badge_number },
-    }) =>
-      violatorFullName.toLowerCase().includes(searchValue.toLowerCase()) ||
-      violatorPassportNumber
-        .toLowerCase()
-        .includes(searchValue.toLowerCase()) ||
-      full_name.toLowerCase().includes(searchValue.toLowerCase()) ||
-      badge_number.toLowerCase().includes(searchValue.toLowerCase())
-  );
+
+  const filterProtocols = (protocols, query) => {
+    const lowerCaseQuery = query.toLowerCase().trim();
+
+    const operator = ["<", ">", "="].find((op) =>
+      lowerCaseQuery.startsWith(op)
+    );
+
+    const amount = parseFloat(lowerCaseQuery.slice(1).trim());
+
+    return protocols.filter((protocol) => {
+      const fineAmount = protocol.fineAmount;
+
+      if (operator) {
+        switch (operator) {
+          case ">":
+            return fineAmount > amount;
+          case "<":
+            return fineAmount < amount;
+          case "=":
+            return fineAmount === amount;
+          default:
+            return false;
+        }
+      }
+
+      return (
+        protocol.violatorFullName.toLowerCase().includes(lowerCaseQuery) ||
+        protocol.violatorPassportNumber
+          .toLowerCase()
+          .includes(lowerCaseQuery) ||
+        protocol.parkOfficer?.full_name
+          .toLowerCase()
+          .includes(lowerCaseQuery) ||
+        protocol.arkOfficer?.badge_number.toLowerCase().includes(lowerCaseQuery)
+      );
+    });
+  };
+
+  const filteredProtocols = filterProtocols(protocols, searchValue);
+
   const protocolsCards = filteredProtocols.map((currentProtocol) => (
     <Protocol key={currentProtocol.id} protocol={currentProtocol} />
   ));
@@ -48,6 +75,7 @@ const ProtocolsPage = () => {
           setSearchValue(value);
         }}
         placeholder="Search"
+        title="Search by fine (e.g., >50, <100, =75) or other criteria"
       />
       {protocolsCards}
     </section>
